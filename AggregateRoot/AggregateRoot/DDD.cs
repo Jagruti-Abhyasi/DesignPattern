3c908fba7aaa0cf48b3fa97b3d8606847a702c15
Jagruti-desktop <jagu-test@gmail.com>
2021-12-15 22:55:00 -0500

    Aggregate root pattern
    Aggregate Root Pattern

---
+++ AggregateRoot/AggregateRoot/DDD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AggregateRoot
+{
+    //Entity Class
+    public class Supplier
+    {
+        // Agrrgeation 1 to many
+        private List<Address> _Addresses { get; set; }
+
+        public void Add(Address obj)
+        {
+            // Add validation here 
+            _Addresses.Add(obj);
+        }
+        //public Dal _dal { get; set; } // ur entity class is using data access layer , which is bad -- Read Note 1)
+        // if ur Dal class will change then supplier class also has to change
+    }
+    public class Address
+    {
+
+    }
+    //Entity Class
+    public class Customer
+    {
+
+    }
+    public class Dal
+    {
+
+    }
+
+}
---
+++ AggregateRoot/AggregateRoot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AggregateRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Supplier obj = new Supplier();
+            Address a = new Address(); // <----- Independently not allowed 
+                                       //obj.Addresses(a); // this is not aggregate root, because you are able to create and manipulate the child object Independently
+                                       // this is not allowed in agrrgeate root    
+            obj.Add(a); // this is agrrgeate root pattern
+        }
+    }
+}
